b245a57fe09785db33d7ceb49d01e4eecf427fe3
ArtemSafarian-Georgian <99339968+ArtemSafarian-Georgian@users.noreply.github.com>
2022-02-09 12:49:58 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,44 +1,27 @@
 ﻿using System;
 
-namespace Problem_set_1
+namespace Question5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            object o  = 5;
-        Console.WriteLine(o.GetType());
-      
-        object V1  = 5.0;
-        Console.WriteLine(V1.GetType());
-
-         object V2  = 5>1;
-        Console.WriteLine(V2.GetType());
-
-        
-         object V3  = '5';
-        Console.WriteLine(V3.GetType());
-
-        
-         object V4  = 5*2;
-        Console.WriteLine(V4.GetType());
-
-        object V5  = 5/2;
-        Console.WriteLine(V5.GetType());
-
-        object V6  = 5%2;
-        Console.WriteLine(V6.GetType());
+            static bool inside(int x, int y, int x1,  int y1, int x2, int y2)
+            {
+                if (x > x1 && x < x2 && y > y1 && y < y2){
+                    return true;
+                         }else{
+                            return false;
+            }
 
        
-
-         object V8  = 5==3;
-        Console.WriteLine(V8.GetType());
-
-        object V9  = (Math.PI);
-        Console.WriteLine(V9.GetType());
-
-        Console.WriteLine("{5,2,1} is an Invalid type ");
-
-        }
     }
+            Console.Write(inside(1,1,0,0,2,3));
+            Console.Write("\n");
+            Console.Write(inside(-1,-1,0,0,2,3));
+
+            
+
 }
+    }
+    }
